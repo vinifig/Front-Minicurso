cb7dad0721d98957fd7bbaf00af0285aaa5b85c4
vinifig <vinifig@hotmail.com>
2016-06-21 06:34:29 -0300

    Projeto finalizado

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-console.log("Olá, Mundo");
+// implementação es5:
+var produtoTemplate = '<div class="card panel panel-default"><div class="panel-heading"><img src="assets/slack.jpg"></div><div class="panel-body"><<product_name>></div></div>'
+
+// implementação es6:
+// const produtoTemplate = `<div class="card panel panel-default">
+//   <div class="panel-heading">
+//     <img src="assets/slack.jpg">
+//   </div>
+//   <div class="panel-body">
+//     <<produto_name>>
+//   </div>
+// </div>`
+
+var inflaProduto = function(produto){
+  var produtoFinal = produtoTemplate.replace("<<produto_name>>", produto.name)
+  $(".list-products").append(produtoFinal)
+}
+$(document).ready(function(){
+  $.ajax({
+    url: 'scripts/data.json', // URL que fornecerá o dado
+    method: "GET" // Verbo HTTP que será utilizado
+  }).done(function(data){ // Listener, ou callback, da nossa consulta
+    console.log(data); // Mostra os dados adquiridos na consulta
+    for(var i = 0; i < data.result.length; i++){
+      inflaProduto(data.result[i]);
+    }
+  })
+})
